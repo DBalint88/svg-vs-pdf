0c8a8bc2f5a5327e0f4293d2922c75d2ae3e1dad
DBalint88 <dbalint88@gmail.com>
2023-04-17 14:53:51 -0400

    use console to look more closely at timestamps

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ button.addEventListener("click", startRace);
 
 function startRace() {
     let svgStart = Date.now();
+    console.log(svgStart)
     svgImage.src = "images/hcb-svg.svg";
     svgTimer.innerText = Date.now() - svgStart;
+    console.log(Date.now());
     let pdfStart = Date.now();
     pdfImage.src = "images/hcb-pdf.pdf" + "#zoom=75";
     pdfTimer.innerText = Date.now() - pdfStart;
